55f4f276c129aa0812d29e1988419f905ccdfec4
Michael Knowlton <mknowlton89@gmail.com>
2021-04-02 16:12:34 -0400

    Worked on card 28 - if someone clicks on a previously searched term in the search history list, it now re-runs the search.

--- assets/js/results.js
+++ assets/js/results.js
@@ -58,6 +58,7 @@ function gettrademMarkApi() {
 
   // Create an un-ordered list for the search history
   const searchHistoryList = $('<ul>');
+  searchHistoryList.attr("id", "searchHistory");
 
   // Append everything to the page
   $('#mySidenav').append(searchHistoryDiv);
@@ -68,10 +69,14 @@ function gettrademMarkApi() {
   // Create a for-loop to build out the full search history
   for (let i = 0; i < searchHistory.length; i++) {
     // Get the item in the array, create a list element, and then give it text.
-    const searchHistoryListItem = $('<li>').text(searchHistory[i]);
+    let searchHistoryListItem = $('<li>');
+    let searchHistoryListLink = $('<a>').text(searchHistory[i]);
+
+    searchHistoryListLink.attr("href", "./results.html?" + searchHistory[i]);
 
     // Append the list item to the list.
     searchHistoryList.append(searchHistoryListItem);
+    searchHistoryListItem.append(searchHistoryListLink);
   }
 
   // fetch request gettrademMarkApi
@@ -150,11 +155,11 @@ function DomainWhoIsinfo() {
 }
 
 
-document.getElementById("start-trademark").addEventListener("click", function() {
+document.getElementById("start-trademark").addEventListener("click", function () {
   window.open('https://www.uspto.gov/trademarks/apply/initial-application-forms', '_blank');
 });
 
-document.getElementById("buy-domain").addEventListener("click", function() {
+document.getElementById("buy-domain").addEventListener("click", function () {
   window.open('  https://www.godaddy.com/', '_blank');
 
 });
